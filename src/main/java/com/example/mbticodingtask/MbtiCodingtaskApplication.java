75fe62a37425327ab6eac4154486648f6d1514d7
JLamprecht <lamprecht.julian@googlemail.com>
2022-05-02 09:14:10 +0200

    All downhill from here...

---
+++ src/main/java/com/example/mbticodingtask/MbtiCodingtaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.mbticodingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiCodingtaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbtiCodingtaskApplication.class, args);
+    }
+
+}
